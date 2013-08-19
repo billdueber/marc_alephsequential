97c6d8a0dc91fb60a6d56738216b80d30743a34b
BillDueber <bill@dueber.com>
2013-08-19 15:24:27 -0400

    Make line_number accessor consistent

--- lib/marc_alephsequential/error.rb
+++ lib/marc_alephsequential/error.rb
@@ -2,11 +2,11 @@ module MARC
   module AlephSequential
     
     class Error < RuntimeError
-      attr_accessor :record_id, :line_no
+      attr_accessor :record_id, :line_number
   
-      def initialize(record_id, line_no)
+      def initialize(record_id, line_number)
         @record_id = record_id
-        @line_no = line_no
+        @line_number = line_number
       end
     end
   end
